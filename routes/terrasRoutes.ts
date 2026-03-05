0e29362048411d4ff89f6e900af4d8cbdd116438
Wissniper <wisdom272006@gmail.com>
2026-03-05 16:01:03 +0100

    updated cafe routers

--- routes/terrasRoutes.ts
+++ routes/terrasRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { getAllTerrasen, getTerrasById } from "../controllers/terrasController";
+import { getAllTerrasen, getTerrasById, getTerrasInArea, getTerrasByIntensity } from "../controllers/terrasController";
 
 const router = express.Router();
 
 router.get("/", getAllTerrasen);
 router.get("/:id", getTerrasById);
+router.get("/search/area/:area", getTerrasInArea);
+router.get("/search/intensity/:intensity", getTerrasByIntensity);
 
 export default router;
