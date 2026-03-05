9d76404646816b2836388b442135da1b81ce941d
Wissniper <wisdom272006@gmail.com>
2026-03-05 18:50:00 +0100

    updated suncalc

--- routes/sunDataRoutes.ts
+++ routes/sunDataRoutes.ts
@@ -4,6 +4,7 @@ import {
   getSunForTerras,
   getSunForRestaurant,
   getCachedSunData,
+  getSunForEvent,
 } from "../controllers/sunDataController";
 
 const router = express.Router();
@@ -11,6 +12,7 @@ const router = express.Router();
 router.get("/:lat/:lng/:time", getSunPosition);
 router.get("/terras/:terrasId", getSunForTerras);
 router.get("/restaurant/:restaurantId", getSunForRestaurant);
+router.get("/event/:eventId", getSunForEvent);
 router.get("/cache/:locationType/:locationId", getCachedSunData);
 
 export default router;
