08cd4b72d2daf3d08af7ede3d9e77c10e8dc3b36
Yoanna <oostyo@gmail.com>
2026-03-05 19:09:48 +0100

    eventModel done

---
+++ models/eventModel.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface EventDocument extends Document {
+    title: string;
+    address: string;
+    date_start: Date;
+    date_end: Date;
+    description?: string; 
+    url?: string;
+    location: {
+        type: string;
+        coordinates: number[];
+    };
+}
+
+const EventSchema = new Schema(
+    {
+        title: {type: String, required: true},
+        address: {type: String, required: true},
+        date_start: {type: Date, required: true},
+        date_end: {type: Date, required: true},
+        description: {type: String},
+        url: {type: String},
+
+        location: {
+            type: {
+                type: String,
+                enum: ["Point"],
+                required: true,
+                default: "Point",
+            },
+            coordinates: {
+                type: [Number],
+                required: true,
+            },
+        },
+
+})
+
+EventSchema.index({ location: '2dsphere' });
+
+const Event = mongoose.model<EventDocument>('Event', EventSchema);
+export default Event;
