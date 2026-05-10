3e3be3706d7fcc1b3786f1e91052c21599d3a7e0
Wissniper <wisdom272006@gmail.com>
2026-05-10 12:41:05 +0200

    clean-up

--- @types/suncalc3/index.d.ts
+++ @types/suncalc3/index.d.ts
@@ -4,9 +4,22 @@ declare module "suncalc3" {
     azimuth: number;
   }
 
+  interface SunTimeEntry {
+    name: string;
+    value: Date;
+    ts: number;
+    pos: number;
+    angle: number;
+    julian: number;
+    valid: boolean;
+  }
+
+  type SunTimes = Record<string, SunTimeEntry | undefined>;
+
   interface SunCalc {
     getPosition(date: Date, lat: number, lng: number): SunPosition;
     getTimes(date: Date, lat: number, lng: number): Record<string, Date>;
+    getSunTimes(date: Date, lat: number, lng: number): SunTimes;
   }
 
   const SunCalc: SunCalc;
